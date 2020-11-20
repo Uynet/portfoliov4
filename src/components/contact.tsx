0db55f211910c135aa38c141e13bc1c598aa3570
uynet <highsate@gmail.com>
2020-11-20 18:27:20 +0900

    contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "../../scss/index.scss"
+import "../../scss/contact.scss"
 import { NavigationBar } from "./navigationBar";
 
 export class Contact extends React.Component{
@@ -10,6 +11,25 @@ export class Contact extends React.Component{
                <div className="header">
                     <div className="contentBar">Contact</div>
                </div>
+               
+               <div className="headline">
+                    <div className="headlineContent">
+                    <div className="contentHead">
+                        お問い合わせ
+                    </div>
+                    </div>
+               </div>
+
+               <div className="contentArea">
+                    <div className="card">
+                        <div className="wrapper">
+                            メールかTwitterにください
+                            <div className="description">
+                                highsate☆gmail☁com
+                            </div>
+                        </div>
+                    </div>
+                </div>
            </React.Fragment>)
     }
 }
