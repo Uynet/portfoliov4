05f1eb3ac674c028b0d4b9b71fc74f606ff2ead3
uynet <highsate@gmail.com>
2020-11-27 08:00:47 +0900

    responsive-profile

--- src/components/Profile/profile.tsx
+++ src/components/Profile/profile.tsx
@@ -14,7 +14,7 @@ export class Profile extends React.Component{
                <div className="headlineContent">
                    <img className="profileIcon" src="./resource/profileIcon.png" height="auto"></img>
                    <div className="biographyArea">
-                        <div className="name">uynet</div>
+                        <span className="name">uynet</span>
                         <div className="biography">1996/12/19生まれ。東京工業大学情報工学科卒業。曲作ったりゲーム作ったりいろいろするなど活動は多岐に渡る。
                         </div>
                     </div>
@@ -25,11 +25,19 @@ export class Profile extends React.Component{
                     <div className="card">
                         <div> 
                             <div className="description">
+                                <span className="cardTitle">プロフィール</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div> 
+                            <div className="description">
                                 <span className="cardTitle">受賞等</span>
                                 <div className="cardContent">
-                                <div> <a className="link" href="https://www.titech.ac.jp/news/2019/043553.html">U22プログラミングコンテスト経済産業大臣賞 (個人開発)</a> </div>
-                                <div> <a className="link" href="https://www.titech.ac.jp/news/2018/043127.html">Google Indie GameFestival 2019 Top10(チーム開発/サウンド)</a> </div>
-                                <div> <a className="link" href="https://chunithm-special.sega.jp/">第三回チュウニズム楽曲コンテスト七海あおい部門 最終候補(作曲)</a> </div>
+                                <li> <a className="link" href="https://www.titech.ac.jp/news/2019/043553.html">U22プログラミングコンテスト経済産業大臣賞 (個人開発)</a> </li>
+                                <li> <a className="link" href="https://www.titech.ac.jp/news/2018/043127.html">Google Indie GameFestival 2019 Top10(チーム開発/サウンド)</a> </li>
+                                <li> <a className="link" href="https://chunithm-special.sega.jp/">第三回チュウニズム楽曲コンテスト七海あおい部門 最終候補(作曲)</a> </li>
                             </div>
                             </div>
                         </div>
@@ -38,14 +46,20 @@ export class Profile extends React.Component{
                     <div className="card">
                         <div className="wrapper">
                             <div className="description">
-                                <span className="cardTitle">ゆいちゃん</span>
+                            <div className="cardTitle">
+                                <span className="cardTitleMain">ゆいちゃん</span>
                                 <span className="cardTitleSub">について</span>
-                                <div className="cardContent">ドーナツが好き。
+                            </div>
+                                <div className="cardContent">この子はゆいちゃんと言います。よろしくおねがいします。ドーナツが好き。
                                 </div>
-                                <div className="credit">イラスト:<a href="http://owarino.xyz" className="link">やいぎ</a>先生</div>
                             </div>
                             <div className="fig">
                                         <img src="./resource/uychan1.png" width={"150px"}/>
+                            </div>
+                            <div className="creditWrapper">
+                                <div className="creditText">
+                                イラスト:<a href="http://owarino.xyz" className="link">やいぎ</a>先生
+                                </div>
                             </div>
                         </div>
                     </div>
